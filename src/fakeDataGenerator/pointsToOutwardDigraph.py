09d0355ceef8c0969a434ac1def5dd34c2c751b8
anorberg <anorberg@metallica.systemsbiology.net>
2012-01-30 16:50:53 -0800

    Graph generation algorithm (using a different graph library, backport later) based on spatial layout of points created with a generator designed to create decent fake interaction networks now in place and seems to be a start, needs to be pruned. Pluggable approach to pruning has stubs, but no pruner implementations yet.

---
+++ src/fakeDataGenerator/pointsToOutwardDigraph.py
@@ -0,0 +1,76 @@
+'''
+Created on Jan 27, 2012
+
+@author: anorberg
+'''
+from __future__ import division
+import numpy.array as ndarray
+import math
+from scipy.spatial import Delaunay
+from pygraph.classes.digraph import digraph
+import itertools
+import yapsy
+
+def euclideanDistance(twople):
+    if len(twople) != 2:
+        raise ValueError("euclideanDistance must take a 2-item tuple containing numeric sequences of equal length")
+    a, b = twople
+    if len(a) != len(b):
+        raise ValueError("euclideanDistance must take a 2-item tuple containing numeric sequences of equal length")
+    
+    ssquare = 0.0
+    
+    for ae, be in zip(a, b):
+        diff = ae - be
+        ssquare += diff * diff
+    
+    return math.sqrt(ssquare)
+
+def graphFromTriangulation(triang, nSeeds):
+    pointTuples = []
+    for point in triang.points: #ndarray
+        pointTuples.append(tuple(point))
+    
+    graph = digraph()
+    for index, point in enumerate(pointTuples):
+        if index < nSeeds:
+            node_color = "red"
+        else:
+            node_color = "black"
+        graph.add_node(point, ("color", node_color))
+    
+    for plex in triang.vertices:
+        for src, dest in itertools.combinations(plex, 2):
+            if src == dest: #no self edges
+                continue
+            elif src > dest: #all edges point down the list
+                src, dest = dest, src;
+            
+            if dest < nSeeds: #seeds can't have incoming edges
+                continue
+            
+            edge = (pointTuples[src], pointTuples[dest])
+            
+            if not graph.has_edge(edge):
+                graph.add_edge(edge, euclideanDistance(edge))
+                
+    return graph
+
+def graphFromPoints(points, nSeeds):
+    return graphFromTriangulation(Delaunay(ndarray(points)), nSeeds)
+
+class IPruneEdges(yapsy.IPlugin):
+    def prune(self, graph):
+        raise NotImplemented("IPruneEdges is a plugin interface. prune MUST be overridden!")
+    
+    @classmethod
+    def implementations(cls, paths):
+        """Return an iterable of plugin-info for every locatable implementation of this interface.
+        """
+        manager = yapsy.PluginManager()
+        manager.setPluginPlaces(paths)
+        manager.setCategoriesFilter({
+            "PruneEdges" : IPruneEdges,                         
+            })
+        manager.collectPlugins()
+        return manager.getPluginsOfCategory("PruneEdges")
