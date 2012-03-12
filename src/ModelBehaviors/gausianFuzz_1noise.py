cf38532f78350d8815a9957759c2f44b749e7ed3
anorberg <anorberg@metallica.systemsbiology.net>
2012-03-12 13:32:07 -0700

    Randomly generate (on a reflected off-center Gaussian curve) the mean and standard deviation of the noise in the gaussianFuzz 1-ary IModelBehavior.

--- src/ModelBehaviors/gausianFuzz_1noise.py
+++ src/ModelBehaviors/gausianFuzz_1noise.py
@@ -10,8 +10,15 @@ import random
 class gaussianFuzz_1noise(IModelBehavior):
     arity = (1, 1)
     isNoise = True
-    STDDEV = 0.5
+    STDDEV_STDDEV = 0.75
+    MEAN_STDDEV = 0.25
+    STDDEV_MEAN = 1
+    
+    def __init__(self):
+        self.mean = random.gauss(0, self.STDDEV_MEAN)
+        self.stddev = abs(random.gauss(self.MEAN_STDDEV, self.STDDEV_STDDEV))
+    
     def calculate(self, value):
-        return value + random.gauss(0, self.STDDEV)
+        return value + random.gauss(self.mean, self.stddev)
     def generate_name(self, parentName):
-        return "gaussian_random(stddev={0})+".format(self.STDDEV) + parentName
+        return "gaussian_random(mean={0}, stddev={1})+".format(self.mean, self.stddev) + parentName
